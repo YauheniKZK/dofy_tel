4522fc3fb181b44820daee64853d60611cbb39e2
Yauheni <yauhenikozak@gmail.com>
2025-12-17 13:05:48 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,26 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import tailwindcss from "@tailwindcss/vite";
-
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, resolve } from 'url';
+import packageJson from './package.json';
+import tailwindcss from '@tailwindcss/vite'
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
+import { dirname } from 'path';
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/dofy_tel/",
-  plugins: [vue(), tailwindcss()],
-  server: {
-    port: 5185,
+  base: '/xogoogo/',
+  plugins: [
+    vue(),
+    tailwindcss(),
+    VueI18nPlugin({
+      include: resolve(dirname(fileURLToPath(import.meta.url)), '../locales')
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   },
-});
+  define: {
+    'import.meta.env.PACKAGE_VERSION': JSON.stringify(packageJson.version)
+  }
+})
